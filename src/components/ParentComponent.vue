e820252f2085c04f5dfd6c8722c898b5b251b7f9
Lukas Cerny <lukascerny@gmx.at>
2022-09-28 16:58:53 +0200

    deleted two childs, added toggle clicked

--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -6,11 +6,9 @@
         <p>{{childText}}</p>
         <li v-for="child in childs" :key="child.id">
             ID: {{child.id}} Text: {{child.childText}}
-        </li>
-        <ChildComponent :parent-text="parentText" @get-child-text="getChildText"></ChildComponent>
-        <ChildComponent :parent-text="parentText" @get-child-text="getChildText"></ChildComponent>
-        <ChildComponent v-for="child in childs" :key="child.childText" :initChildText="child.childText"
-            :childId="child.id" :parent-text="parentText" @get-child-text="getChildText">
+        </li>    
+        <ChildComponent v-for="child in childs" :key="child.childText" :initChildText="child.childText" :clicked="child.clicked"
+            :childId="child.id" :parent-text="parentText" @get-child-text="getChildText" >
 
         </ChildComponent>
     </div>
@@ -27,9 +25,9 @@ export default {
             childText: "",
             childKey: 0,
             childs: [
-                { id: 1, childText: "Child 1" },
-                { id: 2, childText: "Child 2" },
-                { id: 3, childText: "Child 3" }
+                { id: 1, childText: "Child 1", clicked: false },
+                { id: 2, childText: "Child 2", clicked: false },
+                { id: 3, childText: "Child 3", clicked: false }
             ]
         };
     },
@@ -42,7 +40,7 @@ export default {
                 console.log(child.childId);
                 console.log(this.childs[i]);
                 if (child.childId == this.childs[i].id) {
-                    this.childs[i].childText = "clicked";
+                    this.childs[i].clicked = !this.childs[i].clicked;
                 }
             }
         }
